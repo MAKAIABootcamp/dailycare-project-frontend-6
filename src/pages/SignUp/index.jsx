758a4b8ed77ebd7c9ee6a6f6e9ab62715ad4abf6
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-12-19 11:32:35 -0500

    Fix importing error

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,7 +2,6 @@ import { MdOutlineMailOutline, MdOutlineLock  } from 'react-icons/md'
 import { FaRegUser } from 'react-icons/fa6'
 import wallpaper from '../../assets/images/wallpaper-2.png'
 import { Link } from 'react-router-dom'
-import './styles.sass'
 
 const SignUp = () => {
   return (
