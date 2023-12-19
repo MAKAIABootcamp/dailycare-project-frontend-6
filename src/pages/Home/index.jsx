ca399caf059a6b0aa13d849bcf63cd8b29eea59d
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-12-18 23:06:25 -0500

    Initial config and initial styles

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.sass'
+
+const Home = () => {
+  return (
+    <main className='home'>
+      <h1 className='text-3xl font-bold underline'>Home</h1>
+    </main>
+  )
+}
+
+export default Home
