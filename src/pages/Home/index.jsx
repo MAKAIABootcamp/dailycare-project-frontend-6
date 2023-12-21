aae823a612d6c590e94eba154be4e25d15ad74a9
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-12-21 11:42:29 -0500

    Adding Drawer component and starting UserProfile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
+import DrawerAntD from '../../components/Drawer'
 import './styles.sass'
 
 const Home = () => {
   return (
     <main className='home'>
       <h1 className='text-3xl font-bold underline'>Home</h1>
+      <DrawerAntD/>
+      <p>Some contents...</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquid excepturi reprehenderit hic amet? Nisi aliquid error deserunt illum ducimus commodi iusto iure id harum alias aut, assumenda deleniti in!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor modi error, nulla fugiat nam necessitatibus, sit praesentium doloribus ratione maxime similique debitis iste a ipsum hic quae perferendis, non reiciendis?</p>
     </main>
   )
 }
