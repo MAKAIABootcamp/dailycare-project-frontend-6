d4486cb789af17b0c08242ddc3d6379d05935b44
Christian Andrade <ce.andrade.p@gmail.com>
2023-12-29 18:46:00 -0500

    Add new sections on home view and refactor

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import "@splidejs/react-splide/css";
 import Divider from "../../components/Divider";
 const Home = () => {
   return (
-    <main>
+    <main className="homeMain">
       <div className="padding">
         <h1 className="title">
           Bienvenido <span className="title-user">Usuario</span>
@@ -23,6 +23,7 @@ const Home = () => {
         >
           <SplideSlide>
             <div className="img-container">
+              <p className="overText">Correr</p>
               <img
                 className="img"
                 src="src/assets/images/run.jpg"
@@ -32,6 +33,7 @@ const Home = () => {
           </SplideSlide>
           <SplideSlide>
             <div className="img-container">
+              <p className="overText">Estirar</p>
               <img
                 className="img"
                 src="src/assets/images/stretch.png"
@@ -96,9 +98,6 @@ const Home = () => {
           options={{
             type: "loop",
             gap: "2rem",
-            padding: {
-              right: "8rem",
-            },
             pagination: false,
           }}
           aria-label="My Favorite Images"
@@ -106,7 +105,7 @@ const Home = () => {
           <SplideSlide>
             <div className="card altcard">
               <img
-                className="img"
+                className="img todayImage"
                 src="src/assets/images/run.jpg"
                 alt="Image 1"
               />
@@ -120,7 +119,7 @@ const Home = () => {
           <SplideSlide>
             <div className="card altcard">
               <img
-                className="img"
+                className="img todayImage"
                 src="src/assets/images/stretch.png"
                 alt="Image 2"
               />
@@ -133,6 +132,157 @@ const Home = () => {
           </SplideSlide>
         </Splide>
         <h3 className="subtitle">Recomendados para ti</h3>
+        <Splide
+          className="customSplide"
+          options={{
+            type: "loop",
+            gap: "1rem",
+            padding: {
+              right: "10rem",
+            },
+            pagination: false,
+          }}
+          aria-label="My Favorite Images"
+        >
+          <SplideSlide>
+            <div className="card">
+              <div className="img-container">
+                <img
+                  className="img todayImage"
+                  src="src/assets/images/run.jpg"
+                  alt="Image 1"
+                />
+              </div>
+              <div className="card-text">
+                <p className="text-time">5 minutos</p>
+                <p className="text-title">Meditación</p>
+                <p className="text-subtitle">Antiestres</p>
+              </div>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="card">
+              <div className="img-container">
+                <img
+                  className="img todayImage"
+                  src="src/assets/images/stretch.png"
+                  alt="Image 2"
+                />
+              </div>
+              <div className="card-text">
+                <p className="text-time">5 minutos</p>
+                <p className="text-title">Ejercicios de yoga</p>
+                <p className="text-subtitle">Relajación - Antiestres</p>
+              </div>
+            </div>
+          </SplideSlide>
+        </Splide>
+        <Splide
+          options={{
+            type: "loop",
+            gap: "1rem",
+            padding: {
+              right: "10rem",
+            },
+            pagination: false,
+          }}
+          aria-label="My Favorite Images"
+        >
+          <SplideSlide>
+            <div className="card">
+              <div className="img-container">
+                <img
+                  className="img todayImage"
+                  src="src/assets/images/run.jpg"
+                  alt="Image 1"
+                />
+              </div>
+              <div className="card-text">
+                <p className="text-time">5 minutos</p>
+                <p className="text-title">Meditación</p>
+                <p className="text-subtitle">Antiestres</p>
+              </div>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="card">
+              <div className="img-container">
+                <img
+                  className="img todayImage"
+                  src="src/assets/images/stretch.png"
+                  alt="Image 2"
+                />
+              </div>
+              <div className="card-text">
+                <p className="text-time">5 minutos</p>
+                <p className="text-title">Ejercicios de yoga</p>
+                <p className="text-subtitle">Relajación - Antiestres</p>
+              </div>
+            </div>
+          </SplideSlide>
+        </Splide>
+      </div>
+      <Divider />
+      <div className="padding homeFooter">
+        <h3 className="subtitle">Herramientas Gestión del estrés</h3>
+        <div className="cardContainer">
+          <div className="card simpleAltcard">
+            <div className="img-container">
+              <img
+                className="img todayImage"
+                src="src/assets/images/run.jpg"
+                alt="Image 1"
+              />
+            </div>
+            <div className="card-text">
+              <p className="text-time">5 minutos</p>
+              <p className="text-title">Ejercicios de yoga</p>
+              <p className="text-subtitle">Relajación - Antiestres</p>
+            </div>
+          </div>
+          <div className="card simpleAltcard">
+            <div className="img-container">
+              <img
+                className="img todayImage"
+                src="src/assets/images/run.jpg"
+                alt="Image 1"
+              />
+            </div>
+            <div className="card-text">
+              <p className="text-time">5 minutos</p>
+              <p className="text-title">Ejercicios de yoga</p>
+              <p className="text-subtitle">Relajación - Antiestres</p>
+            </div>
+          </div>
+          <div className="card simpleAltcard">
+            <div className="img-container">
+              <img
+                className="img todayImage"
+                src="src/assets/images/run.jpg"
+                alt="Image 1"
+              />
+            </div>
+            <div className="card-text">
+              <p className="text-time">5 minutos</p>
+              <p className="text-title">Ejercicios de yoga</p>
+              <p className="text-subtitle">Relajación - Antiestres</p>
+            </div>
+          </div>
+          <div className="card simpleAltcard">
+            <div className="img-container">
+              <img
+                className="img todayImage"
+                src="src/assets/images/run.jpg"
+                alt="Image 1"
+              />
+            </div>
+            <div className="card-text">
+              <p className="text-time">5 minutos</p>
+              <p className="text-title">Ejercicios de yoga</p>
+              <p className="text-subtitle">Relajación - Antiestres</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
