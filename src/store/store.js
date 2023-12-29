f11c2ce32667968aae9026e941ff61fc965ff0d8
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-12-28 23:47:32 -0500

    Fixing thunk importation

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-
-
+import { thunk } from 'redux-thunk'
+import userReducer from './users/userSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
-  middleware: [thunk],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export default store;
