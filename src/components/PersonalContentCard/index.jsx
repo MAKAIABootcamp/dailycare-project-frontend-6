f77e8e33e58f21dc47129a954d361ab3fa8ac3dd
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2024-01-22 09:33:55 -0500

    adding filter to profile content

--- src/components/PersonalContentCard/index.jsx
+++ src/components/PersonalContentCard/index.jsx
@@ -1,14 +1,13 @@
-import face from '../../assets/images/face.png'
 import './styles.sass'
 
-const PersonalContentCard = () => {
+const PersonalContentCard = ({ details }) => {
   return (
     <article className='personal-content-card'>
-      <img className='personal-content-card__image' src={face} alt="" />
+      <img className='personal-content-card__image' src={details.cardImage} alt="" />
       <div className='personal-content-card__bar'></div>
       <div className='personal-content-card__description'>
-        <h3 className='personal-content-card__description--title'>Escucha activa</h3>
-        <p className='personal-content-card__description--text'>Practica la escucha activa, intenta escuchar, y luego repetir y resumir.</p>
+        <h3 className='personal-content-card__description--title'>{details.title}</h3>
+        <p className='personal-content-card__description--text'>{details.material[0]}</p>
       </div>
     </article>
   )
