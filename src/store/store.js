b7ab3dfc5b224c6a0ced70f4d8acb8305efa2327
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2024-01-22 23:38:16 -0500

    Create new collection for suggestion content

--- src/store/store.js
+++ src/store/store.js
@@ -4,13 +4,15 @@ import userReducer from './users/userSlice'
 import contentReducer from './content/contentSlice'
 import goalReducer from './goals/goalSlice'
 import adminStore from './admin/adminSlice'
+import suggestedContentReducer from './suggestedContent/suggestedContentSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     content: contentReducer,
     goal: goalReducer,
-    admin: adminStore
+    admin: adminStore,
+    suggestedContent: suggestedContentReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 })
